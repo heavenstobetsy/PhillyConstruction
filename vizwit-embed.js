ead24a02a46c6226b2aeb1797a521dc34119b279
heavenstobetsy <e.basney.johnson@gmail.com>
2020-01-21 16:51:58 -0500

    Update vizwit-embed.js

--- vizwit-embed.js
+++ vizwit-embed.js
@@ -1,1 +1,20 @@
 
+<div style="min-height: 230px">
+  <script class="vizwit" type="application/json">
+{
+  "title": "Census Tract",
+  "provider": "carto",
+  "domain": "phl.carto.com",
+  "dataset": "li_unsafe",
+  "chartType": "choropleth",
+  "groupBy": "censustract",
+  "boundaries": "https://phl.carto.com/api/v2/sql?q=select+*+from+census_tracts_2010&format=geojson",
+  "boundariesLabel": "namelsad10",
+  "boundariesId": "name10_padded",
+  "triggerField": "censustract",
+  "baseFilters": [],
+  "filters": {},
+  "aggregateFunction": "count"
+}
+  </script>
+</div>
